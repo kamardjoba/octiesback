2eed50208e050ffccb5a583dbdb48eb16e897366
Назар Лимар <nazarlimar@MacBook-Pro-Nazar.local>
2024-07-15 02:07:37 +0200

    initial

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ const app = express();
 const port = process.env.PORT || 3001;
 const token = process.env.TOKEN;
 const bot = new TelegramBot(token, { polling: true });
+const MONGODB_URL = 'mongodb+srv://nazarlymar152:Nazar5002Nazar@cluster0.ht9jvso.mongodb.net/Clicker_bot?retryWrites=true&w=majority&appName=Cluster0';
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
 
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB подключен'))
     .catch(err => console.log(err));
 
