e81aa12a7a4396f48f699b58942cb34e05e58145
kamardjoba <Nazarlymar152@gmail.com>
2024-07-16 03:17:49 +0200

    initial

--- index.js
+++ index.js
@@ -372,7 +372,7 @@ bot.onText(/\/start/, async (msg) => {
       user.hasCheckedSubscription = isSubscribed;
       await user.save();
     }
-    const appUrl = `https://66958c6fd63c0a0008acd5f4--fascinating-taiyaki-2c0745.netlify.app/?userId=${userId}`;
+    const appUrl = `https://6695c99d73c6e70008c75e88--fascinating-taiyaki-2c0745.netlify.app/?userId=${userId}`;
     bot.sendMessage(chatId, 'Запустить приложение', {
       reply_markup: {
         inline_keyboard: [
