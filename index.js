005cd365368bf88966c1e3aa74de8559e2fe9188
kamardjoba <Nazarlymar152@gmail.com>
2024-07-18 00:30:46 +0200

    initial

--- index.js
+++ index.js
@@ -307,7 +307,8 @@ app.post('/get-coins', async (req, res) => {
 
     let user = await UserProgress.findOne({ telegramId: userId });
     const referralCoins = user.referredUsers.reduce((acc, ref) => acc + ref.earnedCoins, 0);
-    const totalCoins = user.coins + referralCoins;
+    const coins = calculateCoins(accountCreationDate, hasTelegramPremium, isSubscribed);
+    const fuulcoin = coins + referralCoins;
     if (!user) {
       const coins = calculateCoins(accountCreationDate, hasTelegramPremium, isSubscribed);
       user = new UserProgress({ telegramId: userId, nickname, firstName, coins, hasTelegramPremium, hasCheckedSubscription: isSubscribed });
@@ -326,7 +327,7 @@ app.post('/get-coins', async (req, res) => {
     
 
     res.json({
-      coins: totalCoins,
+      coins: fuulcoin,
       referralCoins: referralCoins, // Добавляем общее количество монет за рефералов в ответ
       hasTelegramPremium: user.hasTelegramPremium,
       hasCheckedSubscription: user.hasCheckedSubscription,
@@ -416,7 +417,6 @@ bot.onText(/\/start(?: (.+))?/, async (msg, match) => {
   try {
     let user = await UserProgress.findOne({ telegramId: userId });
     const isNewUser = !user;
-   
     if (isNewUser) {
       const referralCode = generateReferralCode();
       user = new UserProgress({ telegramId: userId, nickname, firstName, coins, hasTelegramPremium, hasCheckedSubscription: isSubscribed, referralCode });
