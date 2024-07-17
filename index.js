b6b8df7ab401e34ce463ee6932f0f85ad1ab4fdb
kamardjoba <Nazarlymar152@gmail.com>
2024-07-17 16:12:35 +0200

    initial

--- index.js
+++ index.js
@@ -309,7 +309,7 @@ app.post('/get-coins', async (req, res) => {
 
     // Добавляем заработанные монеты за рефералов к общему количеству монет пользователя
     const referralCoins = user.referredUsers.reduce((acc, ref) => acc + ref.earnedCoins, 0);
-    const totalCoins = user.coins ;
+    const totalCoins = user.coins + referralCoins;
 
     res.json({
       coins: totalCoins,
