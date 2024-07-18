099ae88a227f3ac1a987b02719a5be5bede34c53
kamardjoba <Nazarlymar152@gmail.com>
2024-07-18 22:41:39 +0200

    initial

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const CHANNEL_ID = -1002187857390;
 
 app.use(cors());
 app.use((req, res, next) => {
-  console.log(Incoming Request: ${req.method} ${req.url});
+  console.log('Incoming Request: ${req.method} ${req.url}');
   next();
 });
 app.use(express.json());
