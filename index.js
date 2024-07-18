b6d5e9059a5c1427508b3e55d5dcf678f4a246b4
kamardjoba <Nazarlymar152@gmail.com>
2024-07-18 03:22:32 +0200

    initial

--- index.js
+++ index.js
@@ -450,7 +450,7 @@ bot.onText(/\/start(?: (.+))?/, async (msg, match) => {
    const imagePath = path.join(__dirname, 'images', 'Octies_bot_logo.png');
     
     console.log(`Sending photo from path: ${imagePath}`);
-    await bot.sendPhoto(chatId, imagePath, { caption: "How cool is your Telegram profile ? Check your rating and receive rewards 🐙" })
+    await bot.sendPhoto(chatId, imagePath)
       .then(() => {
         console.log('Photo sent successfully');
       })
@@ -459,7 +459,7 @@ bot.onText(/\/start(?: (.+))?/, async (msg, match) => {
       });
 
     console.log('Sending message with buttons');
-    bot.sendMessage(chatId, '', {
+    bot.sendMessage(chatId, 'How cool is your Telegram profile ? Check your rating and receive rewards 🐙', {
       reply_markup: {
         inline_keyboard: [
           [
