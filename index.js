060877afc4ac6e84c99dc3f330b6eb8aa8bc4e7b
kamardjoba <Nazarlymar152@gmail.com>
2024-09-04 02:04:37 +0200

    initial

--- index.js
+++ index.js
@@ -270,25 +270,7 @@ app.post('/generate-referral', async (req, res) => {
   }
 });
 
-app.post('/update-mint-status', async (req, res) => {
-  const { userId, hasMintedNFT } = req.body;
 
-  try {
-    let user = await UserProgress.findOne({ telegramId: userId });
-
-    if (!user) {
-      return res.status(404).json({ success: false, message: 'Пользователь не найден.' });
-    }
-
-    user.hasMintedNFT = hasMintedNFT;
-    await user.save();
-
-    res.json({ success: true, message: 'Статус mint обновлен успешно.' });
-  } catch (error) {
-    console.error('Ошибка при обновлении статуса mint:', error);
-    res.status(500).json({ success: false, message: 'Ошибка сервера при обновлении статуса mint.' });
-  }
-});
 
 app.post('/check-subscription', async (req, res) => {
   const { userId } = req.body;
@@ -881,6 +863,15 @@ bot.onText(/\/start(?: (.+))?/, async (msg, match) => {
       }
       if(user.hasTelegramPremium){
          user.coins += 500;
+      }
+      if(user.hasCheckedSubscription2){
+        user.coinsSub += 750;
+      }
+      if(user.hasCheckedSubscription3){
+        user.coinsSub += 750;
+      }
+      if(user.hasCheckedSubscription4){
+        user.coinsSub += 750;
       }
 
       user.nickname = nickname;
